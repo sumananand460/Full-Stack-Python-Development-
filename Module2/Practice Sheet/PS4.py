1c32f5e8f29aefbbaaf90785734720e8ae04953c
Suman Anand <sumananand460@gmail.com>
2026-04-09 21:12:44 +0530

    Added new file

---
+++ Module2/Practice Sheet/PS4.py
@@ -0,0 +1,13 @@
+# Indexing in python
+
+s = "PythonProgramming"
+
+s = "PythonProgramming"
+
+print(s[0])      # 'P'
+print(s[-1])     # 'g' (last character)
+print(s[0:6])    # 'Python'
+print(s[:6])     # 'Python' (from start)
+print(s[6:])     # 'Programming'
+print(s[::2])    # Every 2nd character: 'PtoPormig'
+print(s[::-1])   # Reverse the string
