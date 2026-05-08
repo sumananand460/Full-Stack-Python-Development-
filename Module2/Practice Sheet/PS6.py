5efb9c798364d292ac68eb6ea46eb609e0dd39f1
Suman Anand <sumananand460@gmail.com>
2026-05-08 07:29:09 +0530

    Added new file

---
+++ Module2/Practice Sheet/PS6.py
@@ -0,0 +1,5 @@
+# Concept of Slicing:
+# Slicing is used to extract a particular part from an ordered sequence - list, tuples, string
+a = ["Suman", 21, "Ice Cream", "Bike", 8.14]
+print(a[::-1])  # [8.14, 'Bike', 'Ice Cream', 21, 'Suman']
+print(a[1:4:1]) # [21, 'Ice Cream', 'Bike']
