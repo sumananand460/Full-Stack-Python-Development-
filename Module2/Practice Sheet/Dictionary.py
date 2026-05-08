698f9cae8a7ce1d6287effff6efb93eaf5785f29
Suman Anand <sumananand460@gmail.com>
2026-05-08 07:35:35 +0530

    Added new file

---
+++ Module2/Practice Sheet/Dictionary.py
@@ -0,0 +1,12 @@
+# Dictionary: 
+# Dictionary is a collection of underordered key : value pairs.
+car = {
+    "brand" : "Honda",
+    "model" : "City",
+    "color" : "Black",
+    "year" : 2026
+}
+
+print(car["brand"])
+car["color"] = "Red"    # Values can be changed
+print(car["color"])
