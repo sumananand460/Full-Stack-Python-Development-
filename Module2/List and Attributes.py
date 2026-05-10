ed03373b1a45288c94f7417347f2a936485e2bd7
Suman Anand <sumananand460@gmail.com>
2026-05-10 20:26:39 +0530

    Added a file

---
+++ Module2/List and Attributes.py
@@ -0,0 +1,11 @@
+"""
+What is a list?
+
+=> A list is a mutable sequence of individual items, separated by commas and enclosed within 
+a square bracket.
+
+Attributes: 
+* append(element)
+* insert
+* extend
+"""
