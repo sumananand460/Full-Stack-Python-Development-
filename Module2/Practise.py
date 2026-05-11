6b9527660fcf676fd6298d82c9a0b2e4fac872e2
Suman Anand <sumananand460@gmail.com>
2026-05-11 19:14:16 +0530

    Modified new file

--- Module2/Practise.py
+++ Module2/Practise.py
@@ -4,8 +4,26 @@
 # "if" statment
 # => Works when if the condition is true
 
+'''
 age = int(input("Enter your age: "))
 if age <= 18:
     print("You are eligible to vote")
 else:
     print("You are not eligible to vote")
+'''
+'''
+# Example 2:
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 80:
+    print("You've secured 'A'.")
+elif marks >= 60:
+    print("You've secured 'B'.")
+elif marks >= 50:
+    print("You've secured 'C'.")
+elif marks >= 30:
+    print("You've secured 'D'.")
+else: 
+    print("Sorry failed the test!")
+'''
