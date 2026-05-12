514997397874bbab9bcfb98c471574e075855acd
Suman Anand <sumananand460@gmail.com>
2026-05-12 21:58:23 +0530

    Added new file

---
+++ Module1/Practice Sheet/PS17.py
@@ -0,0 +1,13 @@
+# Loops:
+
+''''
+for i in range (10,0,-1):
+    print(i)
+print("Blast off!")
+'''
+
+# Printing a table using for loop:
+num = int(input("Enter your number: "))
+
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
