4fac88c762eefcda73815c9149ddd89c7ce65d02
Suman Anand <sumananand460@gmail.com>
2026-07-08 11:55:08 +0530

    Added new file

--- Functions/Q1.py
+++ Functions/Q1.py
@@ -1,1 +1,10 @@
 # Reverse a string without slicing
+def reverse_string(s):
+    result = ""
+    for char in s:
+        result = char + result  # Prepend each character
+    return result
+
+# Test
+print(reverse_string("Python"))  # nohtyP
+print(reverse_string("hello"))   # olleh
