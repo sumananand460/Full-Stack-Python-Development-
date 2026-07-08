3836cb8b48bead12af7769d47b46d11e3cf13ac9
Suman Anand <sumananand460@gmail.com>
2026-07-08 11:34:00 +0530

    Added new file

---
+++ Functions/Functions1.py
@@ -0,0 +1,15 @@
+# What is function?
+# -> Function is a reusuable block of code to perform a specific task.
+# You define a fucntion once and declare it anywhere.
+
+def greet(name):
+    return f"Hello World! My name is {name}!"
+
+print(greet("Alice"))
+
+# Types of functions
+"""
+1. Built-in Function: Predefined functions. Ex- len(), print(), max()
+2. User-defined Function: Created by the user. Ex- def my_fucntion():
+3. Lambda Functions: Anonymous Function or Single-Expression. Ex- Lambda x: x^2
+"""
