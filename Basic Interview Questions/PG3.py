6f957571a96cfbc2dcf54c674f29740f0788d825
Suman Anand <sumananand460@gmail.com>
2026-07-09 10:51:36 +0530

    Modified new file

--- Basic Interview Questions/PG3.py
+++ Basic Interview Questions/PG3.py
@@ -1,1 +1,13 @@
 # WAP to check if a number is prime or not
+number = int(input("Enter a number: "))
+
+if number <= 1:
+    print("Not Prime")
+
+else:
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
+            print("Not Prime")
+            break
+    else:
+        print("Prime")
