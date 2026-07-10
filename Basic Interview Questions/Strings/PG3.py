4bd0405d920055c773ee3ff0aca251a94eda8f06
Suman Anand <sumananand460@gmail.com>
2026-07-10 12:25:38 +0530

    Modified new file

---
+++ Basic Interview Questions/Strings/PG3.py
@@ -0,0 +1,6 @@
+# Question 3: Convert to Uppercase/Lowercase
+text = input("Enter your string: ")
+print(text.upper()) # for uppercase
+print(text.lower()) # for lowercase
+print(text.title()) # for title case
+print(text.swapcase()) # for swapcase - first letter is in lowercase
