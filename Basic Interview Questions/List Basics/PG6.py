df6d641aaa96c0197583951b9f769e8dc54b1617
Suman Anand <sumananand460@gmail.com>
2026-07-14 11:20:40 +0530

    Added new file

---
+++ Basic Interview Questions/List Basics/PG6.py
@@ -0,0 +1,19 @@
+# WAP to find the second largest number from the list
+
+def second_largest(arr):
+    if len(arr) < 2:
+        return None
+
+    largest = second = float('-inf')
+
+    for num in arr:
+        if num > largest:
+            second = largest
+            largest = num
+        elif num > second and num!= largest:
+            second = num
+
+    return second if second!= float('-inf') else None
+
+nums = [12, 35, 1, 10, 34, 1]
+print(second_largest(nums)) # 34
